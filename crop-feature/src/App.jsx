9437d41432d5a52ac1edbe8068df475b3e920b88
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2024-02-13 23:06:56 +0530

    Added the image crop feature

--- crop-feature/src/App.jsx
+++ crop-feature/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ImageCrop from './components/ImageCrop';
+import ImageCropProvider from './providers/ImageCropProvider';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <div className="bg-gray-100 h-screen flex justify-center items-center">
+      <ImageCropProvider>
+        <ImageCrop />
+      </ImageCropProvider>
+    </div>
   )
 }
 
